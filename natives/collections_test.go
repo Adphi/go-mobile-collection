7c576684736d8d5ffde79eba38aeda602244aa13
Adphi <philippe.adrien.nousse@gmail.com>
2019-03-14 09:20:25 +0100

    added native collections build

--- natives/collections_test.go
+++ natives/collections_test.go
@@ -1,16 +1,1 @@
 package natives
-
-//func TestStringCollection(t *testing.T){
-//	exp := []string{"1", "2", "3"}
-//	c := NewStrCollectionFrom("1", "2", "3")
-//	it := c.Iterator()
-//	for it.HasNext() {
-//		_, err := it.Next()
-//		assert.NoError(t, err)
-//	}
-//	for i:=0; i < c.Count(); i++ {
-//		s, err := c.Get(i)
-//		assert.NoError(t, err)
-//		assert.Equal(t, exp[i], string(s))
-//	}
-//}
